389e9853f1644b6a22e308155756286febdfc514
CowWowMoo <kelseycgardner@gmail.com>
2021-01-07 15:30:44 -0800

    Header and Footer

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+
+class Header extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Jumbotron fluid>
+                    <div className="container">
+                        <div className="row">
+                            <didv className="col">
+                                <h1>Nucamp</h1>
+                                <h2>a better way to camp</h2>
+                            </didv>
+                        </div>
+                    </div>
+                </Jumbotron>
+                <Navbar dark sticky="top">
+                <div className="container">
+                    <NavbarBrand href="/">NuCamp</NavbarBrand>
+                </div>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
